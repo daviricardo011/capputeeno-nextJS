a1bd1a6ff501a8d3849dc9f1775e3dc3f1bd7b60
Davi Ricardo <davi.ricardo@csptech.com.br>
2025-03-25 10:13:33 -0300

    improve performance

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,27 +1,26 @@
 import BackMenu from "@/components/BackMenu";
 import { IProduct, products } from "@/mocks/products";
 import ProductDetails from "./components/ProductDetails";
+import { notFound } from "next/navigation";
 
 interface Props {
   params: { id: string };
 }
-
-const defaultProduct = {
-  id: 0,
-  src: "",
-  name: "",
-  price: 0,
-  category: "",
-  createdAt: "",
-  description: "",
-  quantitySold: 0,
-};
+export function generateStaticParams() {
+  return products.map((prod) => ({ id: String(prod.id) }));
+}
+export const revalidate = 60;
 
 export default async function ProductPage({ params }: Props) {
   const resolvedSearchParams = await params;
   const productId = Number(resolvedSearchParams?.id);
-  const selectedProduct: IProduct =
-    products.find((prod) => prod.id === productId) || defaultProduct;
+  const selectedProduct: IProduct | undefined = products.find(
+    (prod) => prod.id === productId
+  );
+
+  if (!selectedProduct) {
+    notFound();
+  }
 
   return (
     <main>
